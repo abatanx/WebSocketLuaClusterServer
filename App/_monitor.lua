d2eecaf08142a6329f9292513a66b4e5d5e7440d
Takao Kobayashi <abatanx@gmail.com>
2021-03-02 22:28:28 +0900

    Update: User/Pass edit, and disconnect on failed authentication.

--- App/_monitor.lua
+++ App/_monitor.lua
@@ -5,4 +5,7 @@ if  Core.Server.getAppProperty("app.admin.user") ~= nil     and Core.Request.use
 		Core.Server.getAppProperty("app.admin.password") ~= nil and Core.Request.password == Core.Server.getAppProperty("app.admin.password") then
 	print("Administrative mode")
 	Core.Server.shared().addMonitor(Core.WebSocket.new())
+else
+	print("Failed")
+	Core.WebSocket.new():close()
 end
